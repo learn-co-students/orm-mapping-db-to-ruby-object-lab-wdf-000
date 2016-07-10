6ae55f830cbe7ebbd4767be4ac6b3790a0550c6d
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-10 14:12:19 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,19 +2,75 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    students = DB[:conn].execute("SELECT * FROM students;")
+    students.map {|student| self.new_from_db(student)}
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL 
+	SELECT * FROM students
+	WHERE name = ?;
+      SQL
+    self.new_from_db(DB[:conn].execute(sql, name)[0])  
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade='9';
+    SQL
+    students = DB[:conn].execute(sql)
+    students.map {|student| self.new_from_db(student)}
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+	SELECT * FROM students
+	WHERE grade < 12;
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+	SELECT * FROM students
+	WHERE grade = ?;
+      SQL
+    DB[:conn].execute(sql, grade).map {|student| self.new_from_db(student)}
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+	SELECT * FROM students
+	WHERE grade = '10'
+	ORDER BY id
+	LIMIT 1;
+      SQL
+    self.new_from_db(DB[:conn].execute(sql)[0])
   end
   
+  def self.first_x_students_in_grade_10(x)
+    sql = <<-SQL
+	SELECT * FROM students
+	WHERE grade = '10'
+	ORDER BY id
+	LIMIT ?;
+      SQL
+    DB[:conn].execute(sql, x).map {|student| self.new_from_db(student)}
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
