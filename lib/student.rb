75b6d33fc617555ac4ecd759d1e5ec34bea22c85
aehmt <calisahmet@gmail.com>
2016-07-25 17:30:35 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,13 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
@@ -11,8 +16,9 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1" 
+    row = DB[:conn].execute(sql, name).flatten
+    self.new_from_db(row) 
   end
   
   def save
